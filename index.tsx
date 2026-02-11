201c6058a1d939534b3190b8b5c400a79db1cca6
Evereste <gabreldesign@gmail.com>
2026-02-11 12:56:58 -0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' // Importa o App sem chaves
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// Garante que o elemento root existe antes de tentar renderizar
+const rootElement = document.getElementById('root')
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+} else {
+  console.error("ERRO CRÍTICO: Não encontrei a div com id 'root' no index.html")
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
